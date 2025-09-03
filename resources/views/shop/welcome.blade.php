055f89616c4c03b73e9c8b331bc90cc2b409e232
Ansab Naif <ansab20556@gmail.com>
2025-09-03 22:02:33 +0300

    feat: Implement MVC for shop welcome page

---
+++ resources/views/shop/welcome.blade.php
@@ -0,0 +1,13 @@
+{{-- resources/views/shop/welcome.blade.php --}}
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Shoppatty</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body style="font-family: system-ui; margin: 40px;">
+    <h1>Welcome to Shoppatty!</h1>
+    <p>This is the first step in our E-commerce Store.</p>
+</body>
+</html>
